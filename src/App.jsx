494ccaa42ddec610212e04a609fb16689d024c16
Puttapong <puttapong.lekk@gmail.com>
2024-06-05 11:55:20 +0700

    feat(App.jsx):Add useState to display GreetingMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => setGreetingMessage("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => setGreetingMessage("Hi!")}>Hi!</button>
+        <button onClick={() => setGreetingMessage("你好!")}>你好!</button>
       </div>
     </div>
   );
