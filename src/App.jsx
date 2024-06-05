bafaf54485a201ca5c42bed60ce394f34de54282
Athikhom_ra <athikhom_ra@outlook.com>
2024-06-05 12:32:03 +0700

    Assignment 'react state' NARu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        {/* {solution 1} */}
+        <button
+          onClick={() => {
+            setMessage("สวัสดี!");
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setMessage("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setMessage("你好!");
+          }}
+        >
+          你好!
+        </button>
       </div>
     </div>
   );
